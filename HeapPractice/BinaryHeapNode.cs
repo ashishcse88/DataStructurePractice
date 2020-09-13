453de59b183e0df378fa767d3db2dfb60a0307ae
ashish <ashish.cse88@hotmail.com>
2020-09-13 19:57:41 +0530

    HeapInitialCHeck-in

---
+++ HeapPractice/BinaryHeapNode.cs
@@ -0,0 +1,10 @@
+public class BinaryHeapNode
+{
+    int[] arr;
+    int sizeOfHeap;
+    public BinaryHeapNode(int size)
+    {
+        int[] arr = new int[size];
+        this.sizeOfHeap = 0;
+    }
+}
